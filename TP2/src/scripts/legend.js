7a847128b9897aab0bf47010fffd05c41aea307f
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-05-09 19:48:10 -0400

    added and completed legend.js Todo's but need to check

--- TP2/src/scripts/legend.js
+++ TP2/src/scripts/legend.js
@@ -8,4 +8,14 @@ export function draw (data, color) {
   // TODO : Generate the legend in the div with class "legend". Each SVG rectangle
   // should have a width and height set to 15.
   // Tip : Append one div per legend element using class "legend-element".
+  const legend = d3.select('.legend')
+  legend.selectAll('.legend-element').remove()
+  const legendElements = legend.selectAll('.legend-element').data(data)
+    .enter()
+    .append('div')
+    .attr('class', 'legend-element')
+    .style('display', 'flex')
+    .style('align-items', 'center')
+    .style('margin-right', '10px')
+  legendElements.append('svg')
 }
