73754c296eb09458abf21482c33783e50eb66a85
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-01 09:51:48 -0400

    feat: updated viz.js code

--- TP4/src/scripts/viz.js
+++ TP4/src/scripts/viz.js
@@ -1,3 +1,5 @@
+import { getContents } from './tooltip'
+
 /**
  * Positions the x axis label and y axis label.
  *
@@ -6,7 +8,13 @@
  * @param {number} height The height of the graph
  */
 export function positionLabels (g, width, height) {
-  // TODO : Position axis labels
+  g.select('.x.axis-text')
+    .attr('x', width / 2)
+    .attr('y', height + 40)
+
+  g.select('.y.axis-text')
+    .attr('x', -40)
+    .attr('y', height / 2)
 }
 
 /**
@@ -22,6 +30,21 @@ export function drawCircles (data, rScale, colorScale) {
   // and each circle's color depends on its continent.
   // The fill opacity of each circle is 70%
   // The outline of the circles is white
+  const circles = d3.select('#graph-g')
+    .append('g')
+    .attr('id', 'circles')
+    .selectAll('circle')
+    .data(data)
+
+  circles.enter()
+    .append('circle')
+    .merge(circles)
+    .attr('r', d => rScale(d.Population))
+    .attr('fill', d => colorScale(d.Continent))
+    .style('opacity', 0.7)
+    .attr('stroke', 'white')
+
+  circles.exit().remove()
 }
 
 /**
@@ -32,6 +55,26 @@ export function drawCircles (data, rScale, colorScale) {
 export function setCircleHoverHandler (tip) {
   // TODO : Set hover handler. The tooltip shows on
   // hover and the opacity goes up to 100% (from 70%)
+  const circles = d3.select('#circles').selectAll('circle')
+
+  circles.on('mouseover', (event, data) => {
+    const circle = d3.select(data)._groups[0][0]
+    const content = getContents(circle)
+
+    d3.select(event.currentTarget).style('opacity', 1)
+    tip.offsetX = event.offsetX
+    tip.offsetY = event.offsetY
+    tip.html(content)
+      .style('left', event.pageX + 'px')
+      .style('top', event.pageY + 'px')
+      .style('font-weight', 300)
+      .show(data, event.currentTarget)
+  })
+
+  circles.on('mouseout', (event) => {
+    d3.select(event.currentTarget).style('opacity', 0.7)
+    tip.hide()
+  })
 }
 
 /**
@@ -45,6 +88,12 @@ export function setCircleHoverHandler (tip) {
 export function moveCircles (xScale, yScale, transitionDuration) {
   // TODO : Set up the transition and place the circle centers
   // in x and y according to their GDP and CO2 respectively
+
+  d3.selectAll('#circles circle')
+    .transition()
+    .duration(transitionDuration)
+    .attr('cx', d => xScale(d.GDP))
+    .attr('cy', d => yScale(d.CO2))
 }
 
 /**
@@ -54,4 +103,7 @@ export function moveCircles (xScale, yScale, transitionDuration) {
  */
 export function setTitleText (year) {
   // TODO : Set the title
+  d3.select('#graph-g')
+    .selectAll('.title')
+    .text(`Data for year: ${year}`)
 }
