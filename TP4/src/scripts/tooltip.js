545f72dd7534c00792914a48e487e9f3760252d4
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-01 09:50:53 -0400

    feat: Add tooltip contents generation in tooltip.js

--- TP4/src/scripts/tooltip.js
+++ TP4/src/scripts/tooltip.js
@@ -7,6 +7,28 @@
  * @returns {string} The tooltip contents
  */
 export function getContents (d) {
-  // TODO : Generate tooltip contents
-  return ''
+  const formatNumber = (num) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })
+    return formatter.format(num)
+  }
+
+  const createDataItem = (label, value) => ({ label, value })
+
+  const data = [
+    createDataItem('Country:', d['Country Name']),
+    createDataItem('Population:', d.Population),
+    createDataItem('GDP:', formatNumber(d.GDP) + ' $ (USD)'),
+    createDataItem('CO2 emissions:', formatNumber(d.CO2) + ' metric tonnes')
+  ]
+
+  const createContent = (data) => {
+    return data
+      .map(({ label, value }) => `<div><span class="tooltip-label">${label}</span><span class="tooltip-value">${value}</span></div>`)
+      .join('')
+  }
+
+  return createContent(data)
 }
