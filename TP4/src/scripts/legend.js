526308ca3d3e967ae373ea83ffbb766f3dbce2b8
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-01 07:45:32 -0400

    feat: TP4 init commit

---
+++ TP4/src/scripts/legend.js
@@ -0,0 +1,13 @@
+import d3Legend from 'd3-svg-legend'
+
+/**
+ * Draws the legend.
+ *
+ * @param {*} colorScale The color scale to use
+ * @param {*} g The d3 Selection of the graph's g SVG element
+ * @param {number} width The width of the graph, used to place the legend
+ */
+export function drawLegend (colorScale, g, width) {
+  // TODO : Draw the legend using d3Legend
+  // For help, see : https://d3-legend.susielu.com/
+}
