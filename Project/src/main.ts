fbc025891e670f668696e66b543ea435d6c59e03
ZakariaZair <zakaria.zair@polymtl.ca>
2024-06-08 17:19:22 -0400

    removed useless code

--- Project/src/main.ts
+++ Project/src/main.ts
@@ -1,17 +1,11 @@
+import wpItaly from './assets/wp_italy.jpg'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
     <h1>Vite + TypeScript</h1>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${wpItaly}" class="logo-italy" alt="TypeScript logo" />
+    </a>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -20,5 +14,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
