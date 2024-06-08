fb72edfc3fd6eda3172d6706d2fcc60ddc1cef78
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-07 21:16:42 -0400

    Refactor legend.js to optimize legend rendering and improve code readability

--- TP5/src/scripts/legend.js
+++ TP5/src/scripts/legend.js
@@ -9,4 +9,19 @@ import d3Legend from 'd3-svg-legend'
 export function drawLegend (colorScale, g) {
   // TODO : Generate the legend
   // For help, see : https://d3-legend.susielu.com/
+  const legendGroup = g.append('g')
+    .attr('transform', 'translate(51, 120)')
+    .attr('font-family', 'Open Sans Condensed')
+
+  const legend = d3Legend.legendColor()
+    .title('Légende')
+    .shape('circle')
+    .scale(colorScale)
+    .shapePadding(5)
+    .labelOffset(5)
+    .titleWidth(100)
+    .labelAlign('start')
+    .orient('vertical')
+
+  legendGroup.call(legend)
 }
