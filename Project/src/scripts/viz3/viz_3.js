9885c69a1c3386db5621bf3530694ecb460ce58f
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-06-15 15:46:53 -0400

    feat: viz3 done

--- Project/src/scripts/viz3/viz_3.js
+++ Project/src/scripts/viz3/viz_3.js
@@ -6,8 +6,8 @@ import * as tip from './tooltip'
  * @param width
  */
 export function updateXScale (scale, data, width) {
-  const equipes = data.map(d => { return d.Equipe })
-  scale.domain(equipes)
+  const teams = data.map(d => d.Team)
+  scale.domain(teams)
     .range([0, width])
     .padding([0.2])
 }
@@ -18,7 +18,7 @@ export function updateXScale (scale, data, width) {
  * @param height
  */
 export function updateYScale (scale, data, height) {
-  const max = d3.sum(data, d => d.CJ) / 2
+  const max = d3.max(data, d => d.AverageGoalsPerPlayerByTeam)
   scale.domain([0, max])
     .range([height, 0])
 }
@@ -31,42 +31,37 @@ export function updateYScale (scale, data, height) {
  * @param svg
  */
 export function drawBars (data, color, x, y, svg, width, height, margin) {
-  const subgroups = data.columns.slice(1)
-  const stackedData = d3.stack().keys(subgroups)(data)
-
   svg.append('g')
-    .selectAll('g')
-    .data(stackedData)
-    .enter().append('g')
-    .attr('fill', function (d) { return color(d.key) })
-    .attr('id', function (d) { return color(d.key) })
     .selectAll('rect')
-    .data(function (d) { return d })
+    .data(data)
     .enter()
     .append('rect')
-    .attr('x', function (d) { return x(d.data.Equipe) })
-    .attr('y', function (d) { return y(d[1]) })
-    .attr('height', function (d) { return y(d[0]) - y(d[1]) })
+    .attr('x', d => x(d.Team))
+    .attr('y', d => y(d.AverageGoalsPerPlayerByTeam))
+    .attr('height', d => height - y(d.AverageGoalsPerPlayerByTeam))
     .attr('width', x.bandwidth())
+    .attr('fill', d => d.Team === 'Italy' ? color['Italy'] : color['default'])
     .on('mouseover', tip.tooltip.show)
     .on('mouseout', tip.tooltip.hide)
+    .on('mousemove', function (event, d) {
+      tip.tooltip.show(d, this)
+    })
     
-    svg.append('text')
+  svg.append('text')
     .attr('class', 'x-axis-label')
     .attr('text-anchor', 'middle')
-    .attr('x', width / 2)
-    .attr('y', height + margin.bottom-10)
-    .text('Équipes')
+    .attr('x', width / 2.4)
+    .attr('y', height + margin.bottom - 20)
+    .text('Team')
   
   svg.append('text')
     .attr('class', 'y-axis-label')
     .attr('text-anchor', 'middle')
     .attr('x', -height / 2)
-    .attr('y', -margin.left + 10)
+    .attr('y', -margin.right - 10)
     .attr('dy', '1em')
     .attr('transform', 'rotate(-90)')
-    .text('Nombre de cartons ')
-  
+    .text('Average Goals Per Player')
 
   svg.call(tip.tooltip)
 }
