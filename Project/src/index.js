944abaa2579f1082cda824c8b0a980fe0cbdbabd
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-21 06:47:06 -0400

    chore: Update index.js and .html for the feedback

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,15 +1,15 @@
-import * as performance from "./scripts/viz4/build.js";
-import * as score from "./scripts/viz2/build.js";
-import * as results from "./scripts/viz1/build.js";
-import * as offensive from "./scripts/viz6/build.js";
-import * as cards from "./scripts/viz3/build.js";
-import * as events from "./scripts/viz5/build.js";
-import * as shots from "./scripts/viz7/build.js";
+import * as performance from './scripts/viz4/build.js'
+// import * as score from './scripts/viz2/build.js'
+import * as results from './scripts/viz1/build.js'
+// import * as offensive from './scripts/viz6/build.js'
+import * as cards from './scripts/viz3/build.js'
+import * as events from './scripts/viz5/build.js'
+import * as shots from './scripts/viz7/build.js'
 
-results.build(); // Viz 1
-score.build(); // viz 2
-cards.build(); // Viz 3
-performance.build(); // Viz 4
-offensive.build(); // Viz 6
-events.build(); // Viz 5
-shots.build(); // viz 7
+results.build() // Viz 1
+// score.build() // viz 2
+cards.build() // Viz 3
+performance.build() // Viz 4
+// offensive.build() // Viz 6
+events.build() // Viz 5
+shots.build() // viz 7
