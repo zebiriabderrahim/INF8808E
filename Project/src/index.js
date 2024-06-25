0ef1b37c20a5b9eb66c844dc267fbb4d3a5e6052
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-06-25 18:28:42 -0400

    refactor: Remove viz2 legend and tooltip scripts (not used in project)

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,5 +1,4 @@
 import * as performance from './scripts/viz4/build.js'
-// import * as score from './scripts/viz2/build.js'
 import * as results from './scripts/viz1/build.js'
 import * as offensive from './scripts/viz6/build.js'
 import * as averageGoals from './scripts/viz3/build.js'
@@ -7,7 +6,6 @@ import * as events from './scripts/viz5/build.js'
 import * as passAccuracy from './scripts/viz7/build.js'
 
 results.build() // Viz 1
-// score.build() // viz 2
 averageGoals.build() // Viz 3
 performance.build() // Viz 4
 offensive.build() // Viz 6
