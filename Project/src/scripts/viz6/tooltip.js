9eba62e9f02a9df385d854d93324a2dbfba394c1
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-06-25 18:52:00 -0400

    feat: doc for viz 6

--- Project/src/scripts/viz6/tooltip.js
+++ Project/src/scripts/viz6/tooltip.js
@@ -4,6 +4,11 @@ export const tooltip = d3Tip().attr('class', 'd3-tip').html(function (d) {
   return getContent(d)
 })
 
+/**
+ * Returns the content for the tooltip.
+ * @param {Object} d - The data object.
+ * @returns {string} The tooltip content.
+ */
 function getContent (d) {
   const offAttempts = d.TotalAttemptsOffTarget !== undefined ? d.TotalAttemptsOffTarget : 'N/A'
   const onAttempts = d.TotalAttemptsOnTarget !== undefined ? d.TotalAttemptsOnTarget : 'N/A'
